4ad865c96957ef01b7ff2c78ca1a05aeec40aa75
Sydney Sisco <sydsisco@gmail.com>
2019-12-27 15:24:26 -0800

    output subnet IDs

--- networking/environments/prod/backend.tf
+++ networking/environments/prod/backend.tf
@@ -1,8 +1,7 @@
-# TODO: rename key to: networking.prod.tfstate and check S3 bucket to see if network.tfstate is removed
 terraform {
   backend "s3" {
     bucket = "my-tfstate-files"
-    key    = "network.tfstate"
+    key    = "networking.prod.tfstate"
     region = "us-west-2"
   }
 }
--- networking/environments/prod/main.tf
+++ networking/environments/prod/main.tf
@@ -8,3 +8,11 @@ module "networking" {
 output "vpc_id" {
   value = module.networking.vpc_id
 }
+
+output "public_subnet_ids" {
+  value = module.networking.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.networking.private_subnet_ids
+}
